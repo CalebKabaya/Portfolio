ca94267a92b496c40ad38fc09001e89ff5a51fe1
caleb <calebmutuku4@gmail.com>
2022-08-23 15:15:32 +0300

    added the urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,13 +1,13 @@
 
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.urls import re_path
+from . import views
 
 #..............
 
 urlpatterns=[
-    
-#..............
+        re_path('^$',views.welcome,name = 'index'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
