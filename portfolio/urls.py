71911f08e2674dc01c54c7c60ad449ada657ce53
caleb <calebmutuku4@gmail.com>
2022-08-24 10:13:41 +0300

    made the data migrations

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import re_path,include
 
 urlpatterns = [
-    re_path('admin/', admin.site.urls),
+    re_path('admin', admin.site.urls),
     re_path(r'',include('myapp.urls'))
 
 ]
