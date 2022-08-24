9e1d97a07fd8cb46ea65ff53e3d77f3604301602
caleb <calebmutuku4@gmail.com>
2022-08-24 10:12:08 +0300

    added the views to query the data

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def welcome(request):
-    return render(request, 'index-3.html')
+    homes=Home.objects.all()
+    abouts=About.objects.all()
+    skills=Skills.objects.all()
+    education=Education.objects.all()
+    experiences=Experience.objects.all()
+
+    context= {
+        'homes':homes,
+        'abouts':abouts,
+        'skills':skills,
+        'education':education,
+        'experiences':experiences
+
+    }
+
+    return render(request, 'index-3.html',context)
