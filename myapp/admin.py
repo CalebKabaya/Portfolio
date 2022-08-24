f34420c7fea06fd94428a8c8b73447e5ea248c06
caleb <calebmutuku4@gmail.com>
2022-08-24 10:09:41 +0300

    reqistered the models in the admin file

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Home)
+admin.site.register(About)
+admin.site.register(Skills)
+admin.site.register(Education)
+admin.site.register(Experience)
+
+
+
