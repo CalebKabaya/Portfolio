d134b578be600c70bb4f8de4aad4fc2d0bfa3ef0
caleb <calebmutuku4@gmail.com>
2022-08-24 10:11:28 +0300

    added the views urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,7 +7,8 @@ from . import views
 #..............
 
 urlpatterns=[
-        re_path('^$',views.welcome,name = 'index'),
+    
+    re_path('^$',views.welcome,name = 'index'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
