b6da5640b37596a92143f8143407cacf4c6c6658
caleb <calebmutuku4@gmail.com>
2022-08-28 22:37:21 +0300

    update

--- myapp/urls.py
+++ myapp/urls.py
@@ -9,7 +9,9 @@ from . import views
 urlpatterns=[
     
     re_path('^$',views.welcome,name = 'index'),
-    re_path('projects',views.allprojects,name='projects')
+    re_path('projects',views.allprojects,name='projects'),
+    re_path(r'^singleproject/(?P<project_id>\d+)?$',views.project, name='singleproject'), 
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
