3d52cd88b24d38fbe192f090e21bb43806282654
caleb <calebmutuku4@gmail.com>
2022-08-31 23:25:04 +0300

    added the email function

--- myapp/views.py
+++ myapp/views.py
@@ -3,6 +3,7 @@ from .models import *
 # Create your views here.
 
 def welcome(request):
+        
     homes=Home.objects.all()
     abouts=About.objects.all()
     skills=Skills.objects.all()
@@ -13,6 +14,8 @@ def welcome(request):
     allskills=AllSkills.objects.all()
     testimonials=Testimonials.objects.all()
     address=Address.objects.all()
+    mycvs=MyCV.objects.all()
+    achievments=Achiev.objects.all()
 
     context= {
         'homes':homes,
@@ -24,12 +27,20 @@ def welcome(request):
         'projects':projects,
         'allskills':allskills,
         'testimonials':testimonials,
-        'address':address
-
-
+        'address':address,
+        'mycvs':mycvs,
+        'achievments':achievments
     }
 
-    return render(request, 'index-3.html',context)
+    if request.method=='POST':
+        name=request.POST['name']
+        email=request.POST['email']
+        subject=request.POST['subject']
+        comments=request.POST['comments']
+
+        return render(request,'index-3.html',{'name':name})
+    else:
+        return render(request, 'index-3.html',context)
 
 def allprojects(request):
     projects=Projects.objects.all()
@@ -37,12 +48,10 @@ def allprojects(request):
     abouts=About.objects.all()
 
 
-
     context={
         'projects':projects,
         'address':address,
         'abouts':abouts,
-
 
     }
 
@@ -52,8 +61,6 @@ def project(request,project_id):
     singleproject=Projects.objects.get(id=project_id)
     address=Address.objects.all()
     abouts=About.objects.all()
-
-
 
     context={
         'singleproject':singleproject,
